63395b34109bda2f430ac2f1fbe05d8a36ab7433
Norberto Burciaga <norberto.burciaga@nbsis.com>
2020-01-22 11:31:41 -0600

    c++1z was added to CMake. Router added to hello world

--- webtest.cpp
+++ webtest.cpp
@@ -2,31 +2,80 @@
 #include <restinio/all.hpp>
 
 using namespace std;
+using router_t = restinio::router::express_router_t<>;
 
-restinio::request_handling_status_t handler(restinio::request_handle_t request) {
-	// if method is get and request is "/"
-	if (restinio::http_method_get() == request->header().method() && request->header().request_target() == "/") {
-		request->create_response()
-				.append_header(restinio::http_field::server, "Restinio hello world")
-				.append_header_date_field()
+template < typename RESP >
+RESP init_resp( RESP resp ) {
+	resp.append_header(restinio::http_field::server, "RESTinio sample server /v.0.2");
+	resp.append_header_date_field();
+
+	return resp;
+}
+
+
+auto createServerHandler() {
+	auto router = std::make_unique<router_t>();
+
+	router->http_get(
+		"/",
+		[] (auto req, auto) {
+			init_resp(req->create_response())
 				.append_header(restinio::http_field::content_type, "text/html; charset=utf-8")
-				.set_body("<h1>Hello World!<h1>")
+				.set_body(
+					"<html>\r\n"
+					"  <head><title>Hello from RESTinio!</title></head>\r\n"
+					"  <body>\r\n"
+					"    <center><h1>Hello world</h1></center>\r\n"
+					"  </body>\r\n"
+					"</html>\r\n"
+				)
 				.done();
-		return restinio::request_accepted();
-	} else {
-		return restinio::request_rejected();
-	}
+			return restinio::request_accepted();
+		}
+	);
+
+	router->http_get(
+		"/json",
+		[] (auto req, auto) {
+			init_resp(req->create_response())
+				.append_header(restinio::http_field::content_type, "text/json; charset=utf-8")
+				.set_body(R"-({"message" : "Hello world!"})-")
+				.done();
+			return restinio::request_accepted();
+		}
+	);
+
+	router->http_get(
+		"/plain",
+		[] (auto req, auto) {
+			init_resp(req->create_response())
+				.append_header(restinio::http_field::content_type, "text/plain; charset=utf-8")
+				.set_body("Hello World!")
+				.done();
+			return restinio::request_accepted();
+		}
+	);
+
+	return router;
 }
 
 int main(int argc, char **argv) {
 	cout << "Iniciando web server..." << endl;
 
-	restinio::run(
-		restinio::on_this_thread()
-			.port(8080)
-			.address("localhost")
-			.request_handler(handler)
-	);
+	using traits_t = restinio::traits_t<restinio::asio_timer_manager_t, restinio::single_threaded_ostream_logger_t, router_t>;
+
+	try {
+		restinio::run(
+			restinio::on_this_thread<traits_t>()
+				.port(8080)
+				.address("localhost")
+				.request_handler(createServerHandler())
+		);
+
+	} catch (const exception & ex) {
+		cerr << "Error: " << ex.what() << endl;
+		return 1;
+	}
 	cout << "Terminando web server..." << endl;
 	return 0;
 }
